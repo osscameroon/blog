44752eca7eb8df206a99e701dfcbcc37bd4b08be
Brice Kamhoua <kamhouabrice0@gmail.com>
2024-07-16 01:23:31 +0100

    Added scale effec to tags

--- pages/tags.js
+++ pages/tags.js
@@ -26,7 +26,7 @@ export default function Tags({ tags }) {
           {Object.keys(tags).length === 0 && 'No tags found.'}
           {sortedTags.map((t) => {
             return (
-              <div key={t} className="mt-2 mb-2 mr-5">
+              <div key={t} className="mt-2 mb-2 mr-5 hover:scale-105">
                 <Tag text={t} />
                 <Link
                   href={`/tags/${kebabCase(t)}`}
